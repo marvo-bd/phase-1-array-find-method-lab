0ca15446e3dc01454cd7045f6e492f0ec557a04d
Marvin Kabira <marvobd@gmail.com>
2024-05-06 14:14:21 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+  ];
+  
+  function superbowlWin(record) {
+    const winningRecord = record.find(game => game.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
